41adfca213c969f6ef086c8c42d88b68d78be0ef
Akrima Huzaifa Akhtar <akrimaamirka@gmail.com>
2022-03-15 19:11:41 +0500

    SoundsAdded
    audioclips added

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
     int coins = 0;
+    [SerializeField] Text coinsText;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,7 +14,7 @@ public class ItemCollector : MonoBehaviour
         {
             Destroy(other.gameObject);
             coins++;
-            Debug.Log("Coins: " + coins);
+            coinsText.text = "Coins: " + coins;
         }
     }
 }
